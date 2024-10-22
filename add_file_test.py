42e207200577b479fbbed3289432006840f30397
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-10-22 16:33:54 +0200

    test register all files

--- add_file_test.py
+++ add_file_test.py
@@ -1,4 +1,6 @@
-from modules.add_file import add_file
+from modules.add_file import add_file, register_all_input_files
 
-file_path = "inputs\\vid1_1000.mp4"
-add_file(file_path)
+# file_path = "inputs\\vid1_1000.mp4"
+# add_file(file_path)
+
+register_all_input_files()
