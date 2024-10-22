5b3f3022db33d00d389716980ccff2d68a0729cc
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-10-22 17:53:43 +0200

    clean up

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from modules.tools import get_all_filenames_in_inputs
 
 files = get_all_filenames_in_inputs()
 
-# file = files[2]
-for file in files:
-    run_all_models(file)
+file = files[2]
+run_all_models(file)
+
+# Cycle through all files in inputs directory
+# for file in files:
+#     run_all_models(file)
