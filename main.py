503f99182928086ea66b0a323fcb8d4e14b21b37
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-10-23 10:19:04 +0200

    process single file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from models.yolo11n import yolo11n
 from modules.multi_model import run_all_models
 from modules.tools import get_all_filenames_in_inputs
 
 files = get_all_filenames_in_inputs()
 
 file = files[2]
-run_all_models(file)
+# run_all_models(file)
 
 # Cycle through all files in inputs directory
 # for file in files:
 #     run_all_models(file)
+
+yolo11n.process_file(file)
